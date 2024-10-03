6449d8cfed2fc8ba099dce66bc2d6147a41a958d
kapiso-TW <000617jason@gmail.com>
2024-10-03 23:43:10 +0800

    fix secret

--- secret/secret.js
+++ secret/secret.js
@@ -1,28 +1,29 @@
 const oneTimeHash = "2f1987bf98c09d2f5d2a23a6ae29fa53b9aec8f07ed1330bd439122f5a1a2c2c"; // '1030' 的雜湊值
-        const reusableHash = "a7a39b72f29718e653e73503210fbb597057b7a1c77d1fe321a1afcff041d4e1"; // 'simple' 的雜湊值
-        let isOneTimeUsed = false;
-        async function hashPassword(password) {
-            const encoder = new TextEncoder();
-            const data = encoder.encode(password);
-            const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-            const hashArray = Array.from(new Uint8Array(hashBuffer));
-            return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-        }
+const reusableHash = "a7a39b72f29718e653e73503210fbb597057b7a1c77d1fe321a1afcff041d4e1"; // 'simple' 的雜湊值
+let isOneTimeUsed = false;
+async function hashPassword(password) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+}
 
-        async function unlock() {
-            const passwordInput = document.getElementById("password").value;
-            const errorMessage = document.getElementById("error-message");
+async function unlock() {
+    const passwordInput = document.getElementById("password").value;
+    const errorMessage = document.getElementById("error-message");
 
-            const hashedPassword = await hashPassword(passwordInput);
+    const hashedPassword = await hashPassword(passwordInput);
 
-            if (hashedPassword === reusableHash) {
-                document.getElementById("lock-screen").classList.remove("active");
-                document.getElementById("content").classList.add("active");
-            } else if (hashedPassword === oneTimeHash && !isOneTimeUsed) {
-                isOneTimeUsed = true;
-                document.getElementById("lock-screen").classList.remove("active");
-                document.getElementById("content").classList.add("active");
-            } else {
-                errorMessage.style.display = "block";
-            }
-        }
+    if (hashedPassword === reusableHash) {
+        document.getElementById("lock-screen").classList.remove("active");
+        document.getElementById("content").classList.add("active");
+    } else if (hashedPassword === oneTimeHash && !isOneTimeUsed) {
+        isOneTimeUsed = true;
+        alert(int(isOneTimeUsed))
+        document.getElementById("lock-screen").classList.remove("active");
+        document.getElementById("content").classList.add("active");
+    } else {
+        errorMessage.style.display = "block";
+    }
+}
